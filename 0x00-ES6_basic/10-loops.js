20d36f656ec078fee51721d47011ef4bb8035f8d
craizytech <craizytech@gmail.com>
2024-10-02 12:13:07 +0300

    using the for...of loop to loop through the iterable of the array passed to the function and creating a newarray then adding the value of the array + appendString

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+
+  for (const idx of array) {
+    const value = array[idx];
+    newArray.push(appendString + value);
+  }
+
+  return newArray;
+}
