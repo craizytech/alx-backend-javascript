6623d4d1b5e3e7ffce5ddbbe15df0917df02f465
craizytech <craizytech@gmail.com>
2024-10-03 09:11:21 +0300

    hot fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,11 @@
+// eslint-disable-next-line import/extensions
 import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   const namesPromise = createUser();
   const photoPromise = uploadPhoto();
 
-  Promise.all([namesPromise, photoPromise])
+  return Promise.all([namesPromise, photoPromise])
     .then((values) => console.log(values[1].body, values[0].firstName, values[0].lastName))
     .catch(() => console.log('Signup system offline'));
 }
