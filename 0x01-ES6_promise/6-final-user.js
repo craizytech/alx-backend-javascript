5dbb57ded98bcd0172907e48277ba6b9bb2b144b
craizytech <craizytech@gmail.com>
2024-10-03 11:27:05 +0300

    modified the promise to return the promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,14 +2,10 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
   const uploadPhotoPromise = uploadPhoto(fileName);
 
-  const results = await Promise.allSettled([signUpPromise, uploadPhotoPromise]);
-
-  return results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  }));
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
+    .then((results) => results);
 }
