d663bec0e4ea6477ce460a01a2a998778a184c90
craizytech <craizytech@gmail.com>
2024-10-03 10:39:28 +0300

    handling multiple promises each with different, status of resolving

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/extensions */
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
+    .then((results) => results);
+}
