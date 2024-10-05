300aac7d3dcedf522cb43f497fd8a021b6f3ce02
craizytech <craizytech@gmail.com>
2024-10-05 10:21:45 +0300

    implemented a class named pricing

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,8 +1,43 @@
+/* eslint-disable import/extensions */
 import Currency from './3-currency.js';
 
-
 export default class Pricing {
-    constructor(amount, currency) {
-        this._nams
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  // amount getter method
+  get amount() {
+    return this._amount;
+  }
+
+  // amount setter method
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
     }
+    this._amount = value;
+  }
+
+  // currency getter method
+  get currency() {
+    return this._currency;
+  }
+
+  // currency setter method
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be an instance of Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
