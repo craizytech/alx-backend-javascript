cc0d2f113a0f3dd1a0fca08dafc6ceba6c29e6d1
craizytech <craizytech@gmail.com>
2024-10-09 02:44:02 +0300

    unction named updateUniqueItems that returns an updated map for all items with initial quantity at 1.
    It should accept a map as an argument. The map it accepts for argument is similar to the map you create in the previous task.
    
    For each entry of the map where the quantity is 1, update the quantity to 100. If updating the quantity is not possible (argument is not a map) the error Cannot process should be thrown.

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+export default function updateUniqueItems(mapObj) {
+  if (!(mapObj instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  mapObj.forEach((value, key) => {
+    if (value === 1) {
+      mapObj.set(key, 100);
+    }
+  });
+
+  return mapObj;
+}
